8fb44f654b65767c2b45467a08ab723cebd8ac6d
ProMustafa99 <mustafaksalameh99@gmail.com>
2024-12-15 17:05:18 +0300

    Edit on ErrorMiddleware to send Email

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,15 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
-import { HttpException } from '@/exceptions/httpException';
+import { HttpException } from '@exceptions/httpException';
+import nodemailer from 'nodemailer';
 import { logger } from '@utils/logger';
 
-export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+export const ErrorMiddleware = async (error: HttpException, req: Request, res: Response, next: NextFunction) => {
   try {
-    const status: number = error.status || 500;
-    const message: string = error.message || 'Something went wrong';
+    const status = error.status || 500;
+    const message = error.message || 'Something went wrong';
 
     logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'm.salameh@hagzi.com',
+        pass: 'jjiuryjrgxbqfibs',
+      },
+    });
+
+    const mailOptions = {
+      from: 'm.salameh@hagzi.com',
+      to: 'mustafaksalameh99@gmail.com,yazan@hagzi.com',
+      subject: `Error in API: ${message}`,
+      text: `Error occurred at ${req.path}\nMethod: ${req.method}\nStatus: ${status}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
     res.status(status).json({ message });
-  } catch (error) {
-    next(error);
+
+  } catch (emailError) {
+    next(emailError);
   }
 };
